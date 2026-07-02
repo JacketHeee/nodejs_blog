ad2a83f2b502ce031e494d066a15b7e6c7d5fd8c
Nguyễn Hùng Mạnh <manh.nguyen.e1@nationaldigitaltwin15.com>
2026-07-02 10:37:30 +0700

    feat

--- src/routes/site.js
+++ src/routes/site.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const siteController = require('../app/controllers/SiteController');
+const express = require("express");
+const siteController = require("../app/controllers/SiteController");
 const router = express.Router();
 
-router.get('/', siteController.index);
-router.get('/about', siteController.about);
-router.get('/search', siteController.search);
+router.get("/", siteController.index);
+router.get("/about", siteController.about);
+router.get("/search", siteController.search);
+
+console.log("Con bo biet bay       ");
 
 module.exports = router;
